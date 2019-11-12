736c849f95266a8f7270d8a98647ff6e5ca6d8f4
Dima <ds081181@gmail.com>
2019-11-12 02:24:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
 import './App.css';
+import { store } from './store';
+import { RestaurantsListPage } from './components/RestaurantsListPage';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <Provider store={store}>
+    <Header />
+    <main className="page">
+      <div className="content">
+        <RestaurantsListPage />
+      </div>
+    </main>
+    <Footer />
+  </Provider>
+);
